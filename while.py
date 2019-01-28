345704516e1dc762c1481ab658145a422005f79c
root <h4ry.oop@gmail.com>
2019-01-29 02:02:00 +0700

    least but not last on my code

--- while.py
+++ while.py
@@ -3,25 +3,6 @@
 # Iniriate by HarysMatta - tifosilinux.wordpress.com
 
 
-BARIS = 5
-i = 1
-while i <= BARIS:
-        j = 1
-        for x in range(1, BARIS):
-            print("+ ", end='')
-
-        for l in range(i, 1, -1):
-            print('%d ' % (l*i),end='')
-
-        while j<=i:
-            print('%d ' % (i*j), end='')
-            j+=1
-            
-        print() # new line
-        i+=1
-        BARIS-=1
-print('\b')
-
 BARU = 1
 k = 5
 while k >= BARU:
@@ -31,16 +12,6 @@ while k >= BARU:
             l += 1
         print() # new line
         k-=1
-print("\b")
-
-BARUU = 5
-m = 1
-while m <= BARUU:
-    for n in range(2, m+1):
-        print('%d ' % (m*n), end='')
-    print() # new line
-    m+=1
-
 print()
 
 ##################################################################
@@ -54,3 +25,13 @@ while A <= BOUNDARIES:
         print('%d ' % (A*B), end='')
     print()
     A+=1
+
+print()
+
+BOUNDARIES2 = 5
+C = 1
+while C <= BOUNDARIES2:
+    for D in range(1, C+1):
+        print('%d ' % (C*D), end='')
+    print()
+    C+=1
