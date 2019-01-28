5aae1c770b78bc2fcc146c0b99b322858dfefc3c
root <h4ry.oop@gmail.com>
2019-01-29 02:37:13 +0700

    least but not last on my code

--- while.py
+++ while.py
@@ -3,35 +3,24 @@
 # Iniriate by HarysMatta - tifosilinux.wordpress.com
 
 
-BARU = 1
-k = 5
-while k >= BARU:
-        l = 2
-        while l <= k:
-            print('+ ', end='')
-            l += 1
-        print() # new line
-        k-=1
-print()
-
 ##################################################################
 ##################################################################
 #### FINAL RESULT
-
-BOUNDARIES = 5
-A = 2
-while A <= BOUNDARIES:
-    for B in range(A, 1, -1):
-        print('%d ' % (A*B), end='')
-    print()
-    A+=1
-
 print()
 
-BOUNDARIES2 = 5
-C = 1
-while C <= BOUNDARIES2:
-    for D in range(1, C+1):
-        print('%d ' % (C*D), end='')
+BOUNDARIES = 9
+A = 1
+while A <= BOUNDARIES:
+    for Y in range(2, BOUNDARIES-1):
+        print('+  ',end='')
+
+    for B in range(A, 1, -1):
+        print('%d ' % (A*B), end='')
+
+    for D in range(1, A+1):
+        print('%d ' % (A*D), end='')
+
     print()
-    C+=1
+    A+=1
+    BOUNDARIES-=1
+print()
