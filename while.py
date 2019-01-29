aae7caf329af92624574dda7a7f546e612c9dbc7
root <h4ry.oop@gmail.com>
2019-01-29 19:07:15 +0700

    With Function and Class

--- while.py
+++ while.py
@@ -14,22 +14,25 @@ py_compile.compile('/home/hary/Documents/python/while.py')
 #### FINAL RESULT
 print()
 
-def pyramid():
-    BOUNDARIES = 9
-    A = 1
-    while A <= BOUNDARIES:
-        for Y in range(2, BOUNDARIES-1):
-            print('+  ',end='')
+class Logic:
+    def pyramid(self):
+        BOUNDARIES = 9
+        A = 1
+        while A <= BOUNDARIES:
+            for Y in range(2, BOUNDARIES-1):
+                print('+  ',end='')
 
-        for B in range(A, 1, -1):
-            print('%d ' % (A*B), end='')
+            for B in range(A, 1, -1):
+                print('%d ' % (A*B), end='')
             
-        for D in range(1, A+1):
-            print('%d ' % (A*D), end='')
+            for D in range(1, A+1):
+                print('%d ' % (A*D), end='')
 
-        print()
-        A+=1
-        BOUNDARIES-=1
+            print()
+            A+=1
+            BOUNDARIES-=1
+
+obj = Logic()
+obj.pyramid()
+
 print()
-
-pyramid()
