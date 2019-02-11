cf8f0c0dbf865882b71a5a5e531809a569a53845
root <h4ry.oop@gmail.com>
2019-02-12 04:22:04 +0700

    Last Changes

--- logic/data_structure_selectionsort.py
+++ logic/data_structure_selectionsort.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.5
 
 def selectionsort(alist):
+    print('Keadaan awal: \t\t', alist)
     for i in range(0, len(alist)-1):
-        print('Keadaan awal: \t\t', end='')
-        print(alist)
+        print('Langkah ke-%d: \t' % (i+1), end='')
         minposition = len(alist)-1
         for j in range(len(alist)-2, i-1, -1):
             if alist[j] < alist[minposition]:
@@ -11,6 +11,7 @@ def selectionsort(alist):
         temp = alist[i]
         alist[i] = alist[minposition]
         alist[minposition] = temp
+        print(alist)
 
 data = [56,8,88,1,4,3,17,20,3,87]
 
